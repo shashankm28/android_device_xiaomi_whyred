5b1628bad07f49882276c8ab4fd4f5c2f16b0529
Shashank Mishra <shashank-mishra@hotmail.com>
2018-03-17 21:12:41 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,8 +28,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-# TARGET_CPU_VARIANT := kryo; should be kryo, but no kryo flag available.
-TARGET_2ND_CPU_VARIANT := corex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_BOARD_PLATFORM := sdm660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno509
