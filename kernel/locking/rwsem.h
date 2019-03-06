560c3004dd0e0544fe7c980ae856266719cc90f8
Shashank <shashank-mishra@hotmail.com>
2019-03-06 09:11:17 +0000

    inital commit

---
+++ kernel/locking/rwsem.h
@@ -0,0 +1,24 @@
+#ifdef CONFIG_RWSEM_OPT
+#define RWSEM_READER_OWNED	((struct task_struct *)1UL)
+#endif
+
+#ifdef CONFIG_RWSEM_SPIN_ON_OWNER
+static inline void rwsem_set_owner(struct rw_semaphore *sem)
+{
+	WRITE_ONCE(sem->owner, current);
+}
+
+static inline void rwsem_clear_owner(struct rw_semaphore *sem)
+{
+	WRITE_ONCE(sem->owner, NULL);
+}
+
+#else
+static inline void rwsem_set_owner(struct rw_semaphore *sem)
+{
+}
+
+static inline void rwsem_clear_owner(struct rw_semaphore *sem)
+{
+}
+#endif
